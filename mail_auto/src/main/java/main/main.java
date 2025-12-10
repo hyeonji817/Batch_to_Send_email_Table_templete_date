5c4c7b24d1fcd9b07977feb0069f76a83ba64691
hyeonji817 <guswl930817@gmail.com>
2025-12-10 22:42:05 +0900

    조원우의 첫번째 자지

--- mail_auto/src/main/java/main/main.java
+++ mail_auto/src/main/java/main/main.java
@@ -3,6 +3,7 @@ package main;
 import org.example.EmailSender;
 import org.example.QueryExecutor;
 
+// 조원우 사랑해요요
 public class main {
     public static void main(String[] args) {
         // 데이터베이스 쿼리 실행
